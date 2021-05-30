cdfe1d56faf567809a70664240859f0ef0908e3c
yazuka000 <yazuka.000@gmail.com>
2021-05-30 15:02:41 +0900

    21/5/30 updated

---
+++ AIZU ONLINE JUDGE/DSL-データの集合とクエリ処理/1-集合/1_a-互いに素な集合.cpp
@@ -0,0 +1,98 @@
+/* sample input
+5 12
+0 1 4
+0 2 3
+1 1 2
+1 3 4
+1 1 4
+1 3 2
+0 1 3
+1 2 4
+1 3 0
+0 0 4
+1 0 2
+1 3 0
+
+
+*/
+
+/* 実装方針
+
+*/
+
+#include <bits/stdc++.h> 
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+class DisjointSet{
+    public:
+        vector<int> rank, p;
+
+        DisjointSet(){}
+
+        DisjointSet(int size){
+            rank.resize(size, 0);
+            p.resize(size, 0);
+
+            for(int i=0; i<size; i++) makeSet(i);
+        }
+
+        void makeSet(int x){
+            p[x] = x;
+            rank[x] = 0;
+        }
+
+        bool same(int x, int y){
+            return findSet(x) == findSet(y);
+        }
+
+        void unite(int x, int y){
+            link(findSet(x), findSet(y));
+        }
+
+        void link(int x, int y){
+            if(rank[x] > rank[y]){
+                p[y] = x;
+
+            }else{
+                p[x] = y;
+
+                if(rank[x] == rank[y]){
+                    rank[y]++;
+                }
+            }
+        }
+
+        int findSet(int x){
+            if(x != p[x]){
+                p[x] = findSet(p[x]);
+            }
+
+            return p[x];
+        }
+
+};
+
+int main(){
+    int n, a, b, q;
+    int t;
+
+    cin >> n >> q;
+    DisjointSet ds = DisjointSet(n);
+
+    for(int i=0; i<q; i++){
+        cin >> t >> a >> b;
+
+        if(t == 0) ds.unite(a, b);
+        else if(t == 1){
+            if(ds.same(a, b)) cout << 1 << endl;
+            else cout << 0 << endl;
+        }
+    }
+    
+}
+
+/* 参考回答
+
+*/
