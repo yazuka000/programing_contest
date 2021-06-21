055857f6beb792009550e39fab87dda4cc9815f3
yazuka000 <yazuka.000@gmail.com>
2021-06-22 00:01:51 +0900

    21/6/21 updated

---
+++ Atcoder/ABC/ABC053/A.cpp
@@ -0,0 +1,28 @@
+/* sample input
+
+*/
+
+/* 実装方針
+
+*/
+
+#include <bits/stdc++.h> 
+using namespace std;
+using ll = long long; 
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main(){
+    // 初期値
+    int a;
+
+    // 入力
+
+    // 処理
+
+    // 出力
+    cout << a << endl;
+}
+
+/* 参考回答
+
+*/
